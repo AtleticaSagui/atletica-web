507dcaca0df4e43e03914f358f7ec82debd3ac72
PedroVidalDev <pedrohvidals@gmail.com>
2024-10-10 13:38:10 -0300

    feat: creating default styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "arial";
+  }
+
+  html{
+    font-size: 62.5%;
+  }
+  
+  body{
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 100vh;
+  }
+`
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
